596ccff0bf5068a65baf79452538290f79061f0e
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2019-01-29 09:38:09 +0000

    Added Tests

--- Examples/MotionSensor/src/main/java/il/ac/bgu/cs/bp/bpjs/context/roomsexample/RoomsExample.java
+++ Examples/MotionSensor/src/main/java/il/ac/bgu/cs/bp/bpjs/context/roomsexample/RoomsExample.java
@@ -15,29 +15,29 @@ import il.ac.bgu.cs.bp.bpjs.context.CTX;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 
 public class RoomsExample {
-	public void run(String dbPopulationScript, String persistenceUnit) throws InterruptedException {
-		System.out.println(">>>>>>>>>>>>>>>>>> Rooms example <<<<<<<<<<<<<<<<<<<");
+    public void run(String dbPopulationScript, String persistenceUnit) throws InterruptedException {
+        System.out.println(">>>>>>>>>>>>>>>>>> Rooms example <<<<<<<<<<<<<<<<<<<");
 
-		CTX.init(persistenceUnit);
+        CTX.init(persistenceUnit);
 
-		BProgram bprog = CTX.run(dbPopulationScript, "program.js");
+        BProgram bprog = CTX.run(dbPopulationScript, "program.js");
 
-		// Simulation of external events
-		Thread.sleep(1000);
-		Map<String, Room> rooms = CTX.getContextsOfType("Room.findAll", Room.class)
-				.stream().collect(Collectors.toMap(Room::getId, Function.identity()));
+        // Simulation of external events
+        Thread.sleep(1000);
+        Map<String, Room> rooms = CTX.getContextsOfType("Room.findAll", Room.class)
+                .stream().collect(Collectors.toMap(Room::getId, Function.identity()));
 
-		bprog.enqueueExternalEvent(new MotionDetectedEvent((rooms.get("37/123").getMotionDetector())));
-		Thread.sleep(1000);
-		bprog.enqueueExternalEvent(new MotionStoppedEvent(rooms.get("37/123").getMotionDetector()));
-		Thread.sleep(1000);
-		bprog.enqueueExternalEvent(new MotionDetectedEvent((rooms.get("37/123").getMotionDetector())));
-		//TODO: REMOVED
-		//CTX.close();
-	}
+        bprog.enqueueExternalEvent(new MotionDetectedEvent((rooms.get("37/123").getMotionDetector())));
+        Thread.sleep(1000);
+        bprog.enqueueExternalEvent(new MotionStoppedEvent(rooms.get("37/123").getMotionDetector()));
+        Thread.sleep(1000);
+        bprog.enqueueExternalEvent(new MotionDetectedEvent((rooms.get("37/123").getMotionDetector())));
+        //TODO: REMOVED
+        //CTX.close();
+    }
 
 
-	public static void main(String[] args) throws InterruptedException {
-		new RoomsExample().run("db_population.js", "ContextDB");
-	}
+    public static void main(String[] args) throws InterruptedException {
+        new RoomsExample().run("db_population.js", "ContextDB");
+    }
 }
