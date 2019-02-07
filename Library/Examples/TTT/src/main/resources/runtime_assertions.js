efaba2792eb8c7107e5211922c6ed8fb1969e131
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2019-02-07 11:17:44 +0000

    Revert "Again, reorganized poms In a BOM structure (bill of materials)"
    This reverts commit ba14f6c4

--- Library/Examples/TTT/src/main/resources/runtime_assertions.js
+++
@@ -1,5 +0,0 @@
-CTX.subscribe("assert twice", "Cell", function(c){
-    bp.sync({waitFor: [ createEvent("O",c), createEvent("X",c) ]});
-    bp.sync({waitFor: [ createEvent("O",c), createEvent("X",c) ]});
-    bp.ASSERT(false,"cell marked twice");
-});
