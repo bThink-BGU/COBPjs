c6edcc819872e02aa04c1f43f2d82c51018b0697
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-02-09 00:15:00 +0200

    Updated context backend and room example

--- src/test/java/il/ac/bgu/cs/bp/bpjs/context/Main.java
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/context/Main.java
@@ -8,11 +8,18 @@ import static il.ac.bgu.cs.bp.bpjs.context.PrintCOBProgramRunnerListener.Level;
 public class Main {
   public static void main(final String[] args) {
 
+    /** Choose the desired COBP program... */
     BProgram bprog = new ContextBProgram("SampleContextualProgram.js");
 //    BProgram bprog = new ContextBProgram("chess/dal.js", "chess/bl.js");
 
     final BProgramRunner rnr = new BProgramRunner(bprog);
-//    rnr.addListener(new PrintCOBProgramRunnerListener(Level.ALL));
+
+    /** internal context events are: "CTX.Changed", "_____CTX_LOCK_____", "_____CTX_RELEASE_____"
+     * You can filter these event from printing on console using the Level:
+     * Level.ALL : print all
+     * Level.NONE : print none
+     * Level.CtxChanged: print only CTX.Changed events (i.e., filter the transaction lock/release events)
+     */
     rnr.addListener(new PrintCOBProgramRunnerListener(Level.CtxChanged));
     rnr.run();
   }
