556d6632b83189e65c68951dd0ce9c83b4cb2c54
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-02-15 08:48:30 +0200

    Update to SampleContextualProgram.js

--- src/test/resources/SampleContextualProgram.js
+++ src/test/resources/SampleContextualProgram.js
@@ -1,4 +1,9 @@
 bthread("test", function (){
+  ctx.beginTransaction()
+  // populate initial contextual data
+  ctx.insertEntity("id","type", {})
+  // more ctx.insertEntity...
+  ctx.endTransaction()
   sync({request:bp.Event("haha", 5)})
   sync({request:bp.Event("haha", 3)})
   bp.log.info('entity with id "a5": {0}',ctx.getEntityById('a5'))
@@ -11,6 +16,7 @@ ctx.registerEffect('haha', function(data) {
 
 ctx.registerQuery('B.All', entity=>entity.type=='b')
 ctx.registerQuery('B.<5', entity=>entity.type=='b' && entity.hahaData < 5)
+
 
 ctx.bthread('do something with b objects', 'B.All', function(entity) {
   bp.log.info('{0}: {1}', bp.thread.name, entity)
