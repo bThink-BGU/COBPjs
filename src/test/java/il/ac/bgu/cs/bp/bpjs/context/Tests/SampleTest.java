9995ecece0bc602becf00488ff1b952885b172af
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2023-01-19 13:49:14 +0200

    refactoring

--- src/test/java/il/ac/bgu/cs/bp/bpjs/context/Tests/SampleTest.java
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/context/Tests/SampleTest.java
@@ -13,127 +13,131 @@ import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 
 import il.ac.bgu.cs.bp.bpjs.model.eventselection.PrioritizedBSyncEventSelectionStrategy;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
-    boolean afterUpdatingToWholeDBQuery = false;//this is true when using the whole DB query, and false when using the single query
-    //I.e., when the code in context.js is updated to use the whole DB query, this should be true.
+  boolean afterUpdatingToWholeDBQuery = false;//this is true when using the whole DB query, and false when using the single query
+  //I.e., when the code in context.js is updated to use the whole DB query, this should be true.
 
 
-    /**
-     * Sample test that checks if the only live bthread is the one
-     * that is supposed to be live.(in context)
-     * When only one context is used
-     *
-     * @throws Exception -- if the test fails
-     */
-    @Test
-    public void onlyInContextBthreadsRun() throws Exception {
-        String bprogName = "TestCases/activeOnlyIfInContext.js";
-        if (afterUpdatingToWholeDBQuery) {
-            bprogName = "TestingWholeDbQuery/activeOnlyIfInContext.js";
-        }
-        BProgram bprog = TestUtils.prepareBProgram(bprogName);
-        var res = TestUtils.verify(bprog);
-        assertEquals(2, res.getScannedStatesCount());
-        assertEquals(5, res.getScannedEdgesCount());
+  /**
+   * Sample test that checks if the only live bthread is the one
+   * that is supposed to be live.(in context)
+   * When only one context is used
+   *
+   * @throws Exception -- if the test fails
+   */
+  @Test
+  public void onlyInContextBthreadsRun() throws Exception {
+    String bprogName = "TestCases/activeOnlyIfInContext.js";
+    if (afterUpdatingToWholeDBQuery) {
+      bprogName = "TestingWholeDbQuery/activeOnlyIfInContext.js";
     }
+    BProgram bprog = TestUtils.prepareBProgram(bprogName);
+    var res = TestUtils.verify(bprog);
+    assertEquals(2, res.getScannedStatesCount());
+    assertEquals(5, res.getScannedEdgesCount());
+  }
 
-    /**
-     * Sample test that checks if the only live bthread is the one
-     * that is supposed to be live.(in context)
-     * When two contexts are used
-     *
-     * @throws Exception-- if the test fails
-     */
-    @Test
-    public void onlyInContextBthreadsRun2Contexts() throws Exception {
-        String bprogName = "TestCases/activeOnlyIfInContext_2Contexts.js";
-        if (afterUpdatingToWholeDBQuery) {
-            bprogName = "TestingWholeDbQuery/activeOnlyIfInContext_2Contexts.js";
-        }
-        BProgram bprog = TestUtils.prepareBProgram(bprogName);
-        var res = TestUtils.verify(bprog);
-        assertEquals(8, res.getScannedStatesCount());
-        assertEquals(31, res.getScannedEdgesCount());
+  /**
+   * Sample test that checks if the only live bthread is the one
+   * that is supposed to be live.(in context)
+   * When two contexts are used
+   *
+   * @throws Exception-- if the test fails
+   */
+  @Test
+  public void onlyInContextBthreadsRun2Contexts() throws Exception {
+    String bprogName = "TestCases/activeOnlyIfInContext_2Contexts.js";
+    if (afterUpdatingToWholeDBQuery) {
+      bprogName = "TestingWholeDbQuery/activeOnlyIfInContext_2Contexts.js";
     }
+    BProgram bprog = TestUtils.prepareBProgram(bprogName);
+    var res = TestUtils.verify(bprog);
+    assertEquals(8, res.getScannedStatesCount());
+    assertEquals(31, res.getScannedEdgesCount());
+  }
 
-    /**
-     * Sample test that checks the fact that if a context is always off,
-     * its bthreads are never active
-     *
-     * @throws Exception-- if the test fails
-     */
-    @Test
-    public void bthreadWithOffContextDoesntWakeUp() throws Exception {
-        String bprogName = "TestCases/alwaysOffContext.js";
-        if (afterUpdatingToWholeDBQuery) {
-            bprogName = "TestingWholeDbQuery/alwaysOffContext.js";
-        }
-        BProgram bprog = TestUtils.prepareBProgram(bprogName);
-        var res = TestUtils.verify(bprog);
-        assertEquals(2, res.getScannedStatesCount());
-        assertEquals(5, res.getScannedEdgesCount());
+  /**
+   * Sample test that checks the fact that if a context is always off,
+   * its bthreads are never active
+   *
+   * @throws Exception-- if the test fails
+   */
+  @Test
+  public void bthreadWithOffContextDoesntWakeUp() throws Exception {
+    String bprogName = "TestCases/alwaysOffContext.js";
+    if (afterUpdatingToWholeDBQuery) {
+      bprogName = "TestingWholeDbQuery/alwaysOffContext.js";
     }
+    BProgram bprog = TestUtils.prepareBProgram(bprogName);
+    var res = TestUtils.verify(bprog);
+    assertEquals(2, res.getScannedStatesCount());
+    assertEquals(5, res.getScannedEdgesCount());
+  }
 
-    /**
-     * Sample test that checks the fact that if a context is always off,
-     * its bthreads are never active, even if it is the only context
-     *
-     * @throws Exception-- if the test fails
-     */
-    @Test
-    public void noBthreadInContext() throws Exception {
-        String bprogName = "TestCases/noOneInContext.js";
-        if (afterUpdatingToWholeDBQuery) {
-            bprogName = "TestingWholeDbQuery/noOneInContext.js";
-        }
-        BProgram bprog = TestUtils.prepareBProgram(bprogName);
-        var res = TestUtils.verify(bprog);
-        assertEquals(1, res.getScannedStatesCount());
-        assertEquals(0, res.getScannedEdgesCount());
+  /**
+   * Sample test that checks the fact that if a context is always off,
+   * its bthreads are never active, even if it is the only context
+   *
+   * @throws Exception-- if the test fails
+   */
+  @Test
+  public void noBthreadInContext() throws Exception {
+    String bprogName = "TestCases/noOneInContext.js";
+    if (afterUpdatingToWholeDBQuery) {
+      bprogName = "TestingWholeDbQuery/noOneInContext.js";
     }
+    BProgram bprog = TestUtils.prepareBProgram(bprogName);
+    var res = TestUtils.verify(bprog);
+    assertEquals(1, res.getScannedStatesCount());
+    assertEquals(0, res.getScannedEdgesCount());
+  }
 
-    /**
-     * Sample test that checks the fact that if a context is always off, its bthreads are never active
-     * Even if it is the only context
-     *
-     * @throws Exception -- if the test fails
-     * @comment: this test is not working, because the no way to use priority with context
-     */
-    @Test
-    public void priorityOfTwoBthreads() throws Exception {
-        String bprogName = "TestCases/checkingPriority.js";
-        if (afterUpdatingToWholeDBQuery) {
-            bprogName = "TestingWholeDbQuery/checkingPriority.js";
-        }
-        BProgram bprog = TestUtils.prepareBProgramWithPriority(bprogName);
-        var res = TestUtils.verify(bprog);
-        assertEquals(3, res.getScannedStatesCount());
-        assertEquals(2, res.getScannedEdgesCount());
+  /**
+   * Sample test that checks the fact that if a context is always off, its bthreads are never active
+   * Even if it is the only context
+   *
+   * @throws Exception -- if the test fails
+   * @comment: this test is not working, because the no way to use priority with context
+   */
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  public void priorityOfTwoBthreads(boolean afterUpdatingToWholeDBQuery) throws Exception {
+    String bprogName = "TestCases/checkingPriority.js";
+    if (afterUpdatingToWholeDBQuery) {
+      bprogName = "TestingWholeDbQuery/checkingPriority.js";
     }
+    BProgram bprog = TestUtils.prepareBProgramWithPriority(bprogName);
+    bprog.putInGlobalScope("afterUpdatingToWholeDBQuery", afterUpdatingToWholeDBQuery);
+    var res = TestUtils.verify(bprog);
+    assertEquals(3, res.getScannedStatesCount());
+    assertEquals(2, res.getScannedEdgesCount());
+  }
 
-    /**
-     * Testing using the HotCold Program
-     *
-     * @throws Exception -- if the test fails
-     */
-    @Test
-    public void testHotCold() throws Exception {
-        BProgram bprog;
-        if (afterUpdatingToWholeDBQuery) {
-            bprog = TestUtils.prepareBProgram("TestingWholeDbQuery/HotCold.js");
-        } else {
-            Example example = Example.HotCold;
-            bprog = new ContextBProgram(example.getResourcesNames());
-            example.initializeBProg(bprog);
+  /**
+   * Testing using the HotCold Program
+   *
+   * @throws Exception -- if the test fails
+   */
+  @Test
+  public void testHotCold() throws Exception {
+    BProgram bprog;
+    if (afterUpdatingToWholeDBQuery) {
+      bprog = TestUtils.prepareBProgram("TestingWholeDbQuery/HotCold.js");
+    } else {
+      Example example = Example.HotCold;
+      bprog = new ContextBProgram(example.getResourcesNames());
+      example.initializeBProg(bprog);
 //    BProgram bprog = TestUtils.prepareBProgram("Testing/HotCold");
-        }
-        var res = TestUtils.verify(bprog);
-        assertEquals(115, res.getScannedStatesCount());
-        assertEquals(348, res.getScannedEdgesCount());
     }
+    var res = TestUtils.verify(bprog);
+    assertEquals(115, res.getScannedStatesCount());
+    assertEquals(348, res.getScannedEdgesCount());
+  }
 
 }
--- src/test/resources/TestingWholeDbQuery/activeOnlyIfInContext_2Contexts.js
+++ src/test/resources/TestingWholeDbQuery/activeOnlyIfInContext_2Contexts.js
@@ -2,54 +2,64 @@
 // a b-thread can be active only if it its context is active.
 // Two contexts are used to test this.
 
-let oneEntity = ctx.Entity("Context1", "type", {inContext: true})
-let secondEntity = ctx.Entity("Context2", "type", {inContext: true})
+function query(nane, func) {
+  if (afterUpdatingToWholeDBQuery) {
+    ctx.registerWholeDbQuery('Context1',
+      function (entities) {
+        return entities.filter(e => func(e))
+      })
+  } else {
+    ctx.registerQuery(name, func)
+  }
+}
+
+let oneEntity = ctx.Entity('Context1', 'type', { inContext: true })
+let secondEntity = ctx.Entity('Context2', 'type', { inContext: true })
 ctx.populateContext([oneEntity, secondEntity])
-ctx.registerWholeDbQuery('Context1',
-    function (entities) {
-        return entities.filter(entity => entity.id == 'Context1' && entity.inContext)
-    })
+query('Context1', function (e) {
+  return e => e.id == 'Context1' && e.inContext
+})
 ctx.registerWholeDbQuery('Context2',
-    function (entities) {
-        return entities.filter(entity => entity.id == 'Context2' && entity.inContext)
-    } )
+  function (entities) {
+    return entities.filter(entity => entity.id == 'Context2' && entity.inContext)
+  })
 
 ctx.registerEffect('ToggleContext1', function (data) //can be divided into two effects one for true and one for false
 {
-    ctx.getEntityById('Context1').inContext = !ctx.getEntityById('Context1').inContext
+  ctx.getEntityById('Context1').inContext = !ctx.getEntityById('Context1').inContext
 })
 ctx.registerEffect('ToggleContext2', function (data) //can be divided into two effects one for true and one for false
 {
-    ctx.getEntityById('Context2').inContext = !ctx.getEntityById('Context2').inContext
-} )
-
-bthread('ToggleContext1',function (entity) {
-    while (true) {
-        sync({request: Event('ToggleContext1')})
-    }
+  ctx.getEntityById('Context2').inContext = !ctx.getEntityById('Context2').inContext
 })
-bthread('ToggleContext2',function (entity) {
-    while (true) {
-        sync({request: Event('ToggleContext2')})
-    }
-} )
 
-ctx.bthread("a in context1", "Context1", function (entity) {
-    while (true) {
-        sync({request: Event("a")})
-        bp.ASSERT(entity.inContext, "a in context")
-    }
+bthread('ToggleContext1', function (entity) {
+  while (true) {
+    sync({ request: Event('ToggleContext1') })
+  }
 })
-ctx.bthread("a in context2", "Context2", function (entity) {
-    while (true) {
-        sync({request: Event("a")})
-        bp.ASSERT(entity.inContext, "a in context")
-    }
-} )
-bthread("alwaysOn", function () {
-while (true) {
-        sync({request: Event("b")})
-    }
+bthread('ToggleContext2', function (entity) {
+  while (true) {
+    sync({ request: Event('ToggleContext2') })
+  }
+})
+
+ctx.bthread('a in context1', 'Context1', function (entity) {
+  while (true) {
+    sync({ request: Event('a') })
+    bp.ASSERT(entity.inContext, 'a in context')
+  }
+})
+ctx.bthread('a in context2', 'Context2', function (entity) {
+  while (true) {
+    sync({ request: Event('a') })
+    bp.ASSERT(entity.inContext, 'a in context')
+  }
+})
+bthread('alwaysOn', function () {
+  while (true) {
+    sync({ request: Event('b') })
+  }
 })
 
 
